2c008dcc65d3688943fb14b4478e2ca6e387e68d
Richard <richard@dutton.me.uk>
2015-03-18 19:10:09 +0000

    Fixed issue with using GetWindowRect in Windows 8
    Was returning the wrong size for the window

--- SImpleWindow.Console/Program.cs
+++ SImpleWindow.Console/Program.cs
@@ -5,21 +5,20 @@ namespace SimpleWindow.Console
 {
     class Program
     {
-        public const uint PID = 14340;
         private static void Main(string[] args)
         {
             var processes = ProcessHelper.GetCurrentUsersProcesses();
-            //foreach (var process in processes)
-            //{
-            //    System.Console.WriteLine(process.Id + "-" + process.MainWindowTitle);
-            //}
+            foreach (var process in processes)
+            {
+                System.Console.WriteLine(process.Id + "-" + process.MainWindowTitle);
+            }
 
             var pidString = System.Console.ReadLine();
 
             WindowData window;
             try
             {
-                window = WindowGrabber.CaptureWindow(PID);//Convert.ToUInt32(pidString));
+                window = WindowGrabber.CaptureWindow(Convert.ToUInt32(pidString));
                 window.WindowScreenshot.Save("C:\\test.bmp");
                 Process.Start("C:\\Windows\\system32\\mspaint.exe", "C:\\test.bmp");
             }
--- SimpleWindow/WindowGrabber.cs
+++ SimpleWindow/WindowGrabber.cs
@@ -8,33 +8,64 @@ namespace SimpleWindow
 {
     public static class WindowGrabber
     {
+        private static bool DWMWA_EXTENDED_FRAME_BOUNDS(IntPtr handle, out Rectangle rectangle)
+        {
+            RECT rect;
+            var result = DwmGetWindowAttribute(handle, (int) Dwmwindowattribute.DwmwaExtendedFrameBounds,
+                out rect, Marshal.SizeOf(typeof (RECT)));
+            rectangle = Rectangle.FromLTRB(rect.Left, rect.Top, rect.Right, rect.Bottom);
+            return result >= 0;
+        }
+
+        [DllImport(@"dwmapi.dll")]
+        private static extern int DwmGetWindowAttribute(IntPtr hwnd, int dwAttribute, out RECT pvAttribute, int cbAttribute);
+
+        private enum Dwmwindowattribute
+        {
+            DwmwaExtendedFrameBounds = 9
+        }
+
         [DllImport("user32.dll")]
-        public static extern bool GetWindowRect(IntPtr hWnd, out Rect lpRect);
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
 
         [DllImport("user32.dll")]
         public static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, int nFlags);
+
+        public static Rectangle GetWindowRectangle(IntPtr handle)
+        {
+            if (Environment.OSVersion.Version.Major < 6)
+            {
+                return GetWindowRect(handle);
+            }
+            else
+            {
+                Rectangle rectangle;
+                return DWMWA_EXTENDED_FRAME_BOUNDS(handle, out rectangle)
+                           ? rectangle
+                           : GetWindowRect(handle);
+            }
+        }
+
+        private static Rectangle GetWindowRect(IntPtr handle)
+        {
+            RECT rect;
+            GetWindowRect(handle, out rect);
+            return Rectangle.FromLTRB(rect.Left, rect.Top, rect.Right, rect.Bottom);
+        }
 
         public static WindowData CaptureWindow(uint processId)
         {
             var proc = Process.GetProcessById((int)processId);
             var hWnd = proc.MainWindowHandle;
-
-            var window = new ProcessWindow(proc);
-            window.Focus();
-            window.BringToTop();
-
-            Rect rc;
-            GetWindowRect(hWnd, out rc);
-
-            var bmp = new Bitmap(rc.Width, rc.Height, PixelFormat.Format32bppArgb);
+            var processWindowRectangle = GetWindowRectangle(hWnd);
+            var bmp = new Bitmap(processWindowRectangle.Width, processWindowRectangle.Height, PixelFormat.Format32bppArgb);
             var gfxBmp = Graphics.FromImage(bmp);
             IntPtr hdcBitmap = gfxBmp.GetHdc();
             PrintWindow(hWnd, hdcBitmap, 0);
             gfxBmp.ReleaseHdc(hdcBitmap);
             gfxBmp.Dispose();
 
-            Debug.WriteLine("Grabber : " + rc);
-            return new WindowData { WindowPosition = new Rectangle(rc.X, rc.Y, rc.Width, rc.Height), WindowScreenshot = bmp };
+            return new WindowData { WindowPosition = processWindowRectangle, WindowScreenshot = bmp };
         }
     }
 
