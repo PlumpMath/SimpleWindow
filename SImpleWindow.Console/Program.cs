27ad45e61cf1982f69ecaecd246a603e5cc5318e
Richard <richard@dutton.me.uk>
2015-03-19 18:36:35 +0000

    Removed unused usings

--- SImpleWindow.Console/Program.cs
+++ SImpleWindow.Console/Program.cs
@@ -7,24 +7,27 @@ namespace SimpleWindow.Console
     {
         private static void Main(string[] args)
         {
-            var processes = ProcessHelper.GetCurrentUsersProcesses();
-            foreach (var process in processes)
+            while (true)
             {
-                System.Console.WriteLine(process.Id + "-" + process.MainWindowTitle);
+                var processes = ProcessHelper.GetCurrentUsersProcesses();
+                foreach (var process in processes)
+                {
+                    System.Console.WriteLine(process.Id + "-" + process.MainWindowTitle);
+                }
+
+                var pidString = System.Console.ReadLine();
+
+                WindowData window;
+                try
+                {
+                    window = WindowGrabber.CaptureWindow(Convert.ToUInt32(pidString));
+                    window.WindowScreenshot.Save("C:\\test.bmp");
+                    Process.Start("C:\\Windows\\system32\\mspaint.exe", "C:\\test.bmp");
+                }
+                catch (FormatException)
+                {
+                }
             }
-
-            var pidString = System.Console.ReadLine();
-
-            WindowData window;
-            try
-            {
-                window = WindowGrabber.CaptureWindow(Convert.ToUInt32(pidString));
-                window.WindowScreenshot.Save("C:\\test.bmp");
-                Process.Start("C:\\Windows\\system32\\mspaint.exe", "C:\\test.bmp");
-            }
-            catch (FormatException){ }
-
-            System.Console.ReadLine();
         }
     }
 }
